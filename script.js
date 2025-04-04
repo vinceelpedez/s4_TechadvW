51af6f267a97280ad39108f6dc4b6af8f164f1cf
Vincent Luke Elpedez <202111190@fit.edu.ph>
2025-04-04 22:50:56 +0700

    DOne

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple click handler for the "Main Menu" button
+document.getElementById("main-menu-btn").addEventListener("click", function() {
+    alert("Main Menu clicked!");
+    // Additional interactivity or navigation logic can be added here.
+  });
+  
